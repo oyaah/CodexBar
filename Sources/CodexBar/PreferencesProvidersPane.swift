4becb922d18dd52878df487525bc0c3a1f1190e1
Peter Steinberger <steipete@gmail.com>
2025-12-28 14:46:10 +0100

    style: rebalance divider spacing

--- Sources/CodexBar/PreferencesProvidersPane.swift
+++ Sources/CodexBar/PreferencesProvidersPane.swift
@@ -5,7 +5,7 @@ import SwiftUI
 private enum ProviderListMetrics {
     static let rowSpacing: CGFloat = 12
     static let rowInsets = EdgeInsets(top: 6, leading: 0, bottom: 6, trailing: 0)
-    static let dividerBottomInset: CGFloat = 7
+    static let dividerBottomInset: CGFloat = 10
     static let checkboxSize: CGFloat = 18
     static let iconSize: CGFloat = 18
     static let reorderHandleSize: CGFloat = 12
@@ -447,7 +447,6 @@ private struct ProviderListSectionDividerView: View {
         Rectangle()
             .fill(Color(nsColor: .separatorColor))
             .frame(height: 1)
-            .padding(.vertical, 2)
             .padding(.leading, ProviderListMetrics.reorderHandleSize + ProviderListMetrics.checkboxSize + 14)
             .padding(.trailing, 10)
     }
