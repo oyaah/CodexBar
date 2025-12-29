b60cf3ad8af1caee391b03ce314a4d6fae28342a
vandamd <vandamdinh71@gmail.com>
2025-12-29 20:10:47 +0000

    Update Provider Icons to use SVGs

--- Sources/CodexBar/ProviderBrandIcon.swift
+++ Sources/CodexBar/ProviderBrandIcon.swift
@@ -6,31 +6,13 @@ enum ProviderBrandIcon {
 
     static func image(for provider: UsageProvider) -> NSImage? {
         let baseName = ProviderDescriptorRegistry.descriptor(for: provider).branding.iconResourceName
-        let url1x = Bundle.main.url(forResource: baseName, withExtension: "png")
-        let url2x = Bundle.main.url(forResource: baseName + "@2x", withExtension: "png")
-
-        let image = NSImage(size: self.size)
-        var added = false
-
-        if let url1x,
-           let data1x = try? Data(contentsOf: url1x),
-           let rep1x = NSBitmapImageRep(data: data1x)
-        {
-            rep1x.size = self.size
-            image.addRepresentation(rep1x)
-            added = true
+        guard let url = Bundle.main.url(forResource: baseName, withExtension: "svg"),
+              let image = NSImage(contentsOf: url)
+        else {
+            return nil
         }
 
-        if let url2x,
-           let data2x = try? Data(contentsOf: url2x),
-           let rep2x = NSBitmapImageRep(data: data2x)
-        {
-            rep2x.size = self.size
-            image.addRepresentation(rep2x)
-            added = true
-        }
-
-        guard added else { return nil }
+        image.size = self.size
         image.isTemplate = true
         return image
     }
